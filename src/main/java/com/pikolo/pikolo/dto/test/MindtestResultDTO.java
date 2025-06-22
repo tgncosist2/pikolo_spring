d8601fcb867c8dc5c7b7fee7f2a1cf0047b5c2d8
tgncosist2 <tgncosist2@gmail.com>
2025-06-22 11:29:11 +0900

    MBTI검사 키포인트 추가

--- src/main/java/com/pikolo/pikolo/dto/test/MindtestResultDTO.java
+++ src/main/java/com/pikolo/pikolo/dto/test/MindtestResultDTO.java
@@ -10,6 +10,12 @@ public class MindtestResultDTO {
     private int contentId;
     private int scoreMin;
     private int scoreMax;
+    private String resultKeyPoint1Title;
+    private String resultKeyPoint1Desc;
+    private String resultKeyPoint2Title;
+    private String resultKeyPoint2Desc;
+    private String resultKeyPoint3Title;
+    private String resultKeyPoint3Desc;
     private String resultTitle;
     private String resultText;
 }
